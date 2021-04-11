09448f0d9003cfffd8fca44b379d076ea132329f
Adityakane2001 <adityakane1@gmail.com>
2021-04-11 14:21:10 +0000

    Added model checkpoint

--- maskrcnn_benchmark/data/datasets/answersheet.py
+++ maskrcnn_benchmark/data/datasets/answersheet.py
@@ -10,4 +10,6 @@ class AnswersheetDataset(object):
     def __init__(self,imgs_dir,use_charann=False,gts_dir=None,transforms= None):
         self.image_dir = imgs_dir
         self.use_charann = use_charann
+    
+    
         
